94d9641fe7d393692bb6da4ee4bb54fecb7d5e79
boy <mohammadhoseynzamany@gmail.com>
2023-04-27 00:22:19 +0330

    Improve Home page's styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import Link from "next/link"
-import Image from "next/image"
-import logo from "/public/logo.png"
 
 export default function Header(props) {
 
-    const linkClassName = "sm:border-2  shadow-lg shadow-gray-100 border-b-4 py-3 px-5 mx-3 cursor-pointer hover:bg-orange-300 hover:text-white active:bg-orange-500 active:text-white active:cursor-wait block sm:inline-block rounded-full active:animate-ping"
+    const linkClassName = "bg-orange-100 shadow-lg shadow-gray-100 border-b-4 py-3 px-5 mx-3 cursor-pointer hover:bg-orange-300 hover:text-white active:bg-orange-500 active:text-white active:cursor-wait block sm:inline-block rounded-full active:animate-ping italic"
 
     return (
-    <div className="flex">
+    <div className="flex mt-2 mb-10">
         <div className="m-auto my-3 p-2 inline-block rounded-2xl sm:rounded-full shadow-xl bg-orange-50">
             <Link className={ linkClassName } href="/" for="home">خانه</Link>
             <Link className={ linkClassName } href="/aboutus">درباره ما</Link>
